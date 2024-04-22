a56d24a44d49c00e9a78dfee1cf3dca2525589b0
kavitasuthar123 <115469001+kavitasuthar123@users.noreply.github.com>
2024-04-22 11:37:48 +0000

    datatype

---
+++ 1_basics/1_datatype.js
@@ -0,0 +1,15 @@
+
+/*
+
+1. string 
+2. boolen= true /false;
+3. bigint
+4. null=standalone value
+5. undefined
+6. symbol = unique
+7. object 
+
+
+*/
+
+console.log(typeof null);
--- 1_basics/test.js
+++ 1_basics/test.js
@@ -1,1 +1,14 @@
-console.log("kavita suthar")
+// console.log("kavita suthar")
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+let account =12345;
+var num="nehu";
+let accnum="234h";
+console.log([account,num,accnum]);
